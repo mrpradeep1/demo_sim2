63c89e01f7ba697bdc19389a845c376d637a0d1c
PradeepMohanRao <pradeep.mohanrao@gmail.com>
2023-12-09 14:29:20 +0530

    new addion functionality added

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,10 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		System.out.println("Welcome to DevOps!");
+		int a = 5;
+		int b = 10;
+		int c = a + b;
+		System.out.println("Sum of two numbers:"+ c);
 	}
 }
 
