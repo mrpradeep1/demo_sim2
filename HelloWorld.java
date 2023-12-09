c4b6c5ccb83c99cf41f89b89a488ac92bb91129b
mrpradeep1 <112063754+mrpradeep1@users.noreply.github.com>
2023-12-09 14:49:11 +0530

    Update HelloWorld.java updated with fixes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,13 @@
 public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello World!");
-		System.out.println("Welcome to DevOps!);
+		System.out.println("Welcome to DevOps!");
 		int a = 5;
 		int b = 10;
 		int c = a + b;
-		int d = b - a
+		int d = b - a;
 		System.out.println("Sum of two numbers:"+ c);
-		System.out.println("Difference of two numbers:"+ d)
+		System.out.println("Difference of two numbers:"+ d);
 	}
 }
 
